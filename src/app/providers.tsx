13eca5c107f4cc90a862e01dcdb79eee79b8f6c3
Muhammed Aycetin <muhammed_aycetin@hotmail.com>
2026-05-21 10:34:29 +0200

    tailwind

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,3 @@
-// Providers
-// Wraps App in alle benoetigten Context-Provider.
-// MantineProvider: Mantine-UI-Komponenten.
-// Notifications: globaler Toast-Mechanismus (showNotification ueberall verfuegbar).
-// Spaeter koennen weitere Provider hier dazugehaengt werden.
-
-import { MantineProvider } from '@mantine/core'
-import '@mantine/core/styles.css'
-import { Notifications } from '@mantine/notifications'
-import '@mantine/notifications/styles.css'
 import type { ReactNode } from 'react'
 
 interface ProvidersProps {
@@ -15,10 +5,5 @@ interface ProvidersProps {
 }
 
 export function Providers({ children }: ProvidersProps) {
-  return (
-    <MantineProvider>
-      <Notifications position="bottom-right" />
-      {children}
-    </MantineProvider>
-  )
+  return children
 }
